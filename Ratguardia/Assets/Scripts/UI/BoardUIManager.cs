b06a10ad3b4625d37cc46657e92f6dd3c827c2ef
Gigi Bachtel <gbachtel@ucsc.edu>
2020-07-23 15:06:25 -0700

    Added CardGameUI prefab
    currently just has text displaying the number of cards remaining in the deck

---
+++ Ratguardia/Assets/Scripts/UI/BoardUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BoardUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI deckCount;
+
+    // Currently updates all UI every frame
+    // should be fixed from Board class to only update relevant UI on events
+    void Update()
+    {
+        UpdateDeckUI();
+    }
+
+    public void UpdateDeckUI()
+    {
+        deckCount.text = Board.main.deck.stack.Count + "";
+    }
+}
