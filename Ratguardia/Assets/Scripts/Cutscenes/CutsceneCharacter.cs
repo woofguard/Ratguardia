3d769bc7bb33141075f7a528a42aebab10a41c6b
Gigi Bachtel <gbachtel@ucsc.edu>
2020-08-11 15:54:39 -0700

    Created Cutscene with characters and dialogue parsing

---
+++ Ratguardia/Assets/Scripts/Cutscenes/CutsceneCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CutsceneCharacter : MonoBehaviour
+{
+    public GameObject textbox;
+    public TextMeshProUGUI line;
+
+
+    public void OpenTextbox()
+    {
+        textbox.SetActive(true);
+        line.text = "";
+    }
+
+    public void CloseTextbox()
+    {
+        textbox.SetActive(false);
+        line.text = "";
+    }
+
+    public void SetLine(string text)
+    {
+        line.text = text;
+    }
+}
---
+++ Ratguardia/Assets/Scripts/Cutscenes/ScriptParser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScriptParser : MonoBehaviour
+{
+
+    // load file and return as Dialogue array
+    public Dialogue[] LoadFile(string name)
+    {
+        TextAsset file = Resources.Load(name) as TextAsset;
+        string[] lines = file.text.Split('\n');
+
+        List<Dialogue> dialogues = new List<Dialogue>();
+        for(int i = 2; i < lines.Length; i++) // skip first 2 lines
+        {
+            string line = lines[i];
+            string[] segments = line.Split('	');
+
+            Dialogue dialogue = new Dialogue();
+            dialogue.name = segments[0];
+            dialogue.line = segments[1];
+            dialogue.effect = ""; // right now there are no effects so whatevs
+
+            dialogues.Add(dialogue);
+        }
+
+        return dialogues.ToArray();
+
+    }
+}
+
+public struct Dialogue
+{
+    public string name;
+    public string line;
+    public string effect;
+}
