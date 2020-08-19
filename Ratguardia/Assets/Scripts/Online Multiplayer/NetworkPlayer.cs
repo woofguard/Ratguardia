83a27b59a5d7fd6809955b45ec3f68d8c5a93c23
Gigi Bachtel <gbachtel@ucsc.edu>
2020-08-19 14:02:06 -0700

    Added visual hand to network player

--- Ratguardia/Assets/Scripts/Online Multiplayer/NetworkPlayer.cs
+++ Ratguardia/Assets/Scripts/Online Multiplayer/NetworkPlayer.cs
@@ -27,4 +27,57 @@ public class NetworkPlayer : Player
     {
         yield return null;
     }
+
+    public override void ArrangeHand()
+    {
+        int pIndex = Board.main.GetHumanPlayer().playerIndex;
+
+        int layoutIndex = 1;
+
+        switch(pIndex)
+        {
+            case 0:
+                layoutIndex = playerIndex;
+                break;
+            case 1:
+                layoutIndex = playerIndex - 1;
+                if (layoutIndex == -1) layoutIndex = 3;
+                break;
+            case 2:
+                layoutIndex = playerIndex + 2;
+                if (layoutIndex == 5) layoutIndex = 1;
+                break;
+            case 3:
+                layoutIndex = playerIndex + 1;
+                break;
+            default:
+                Debug.Log("player index outside the array hewwo???");
+                break;
+        }
+
+        Transform layout = transform.Find("Layout" + layoutIndex);
+        Debug.Log(layout);
+        if (layout == null) return;
+        if (!(hand.Count == 5 || hand.Count == 6)) return;
+        try
+        {
+            fiveCardLayout = layout.Find(hand.Count + "CardLayout").gameObject;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString()); // puts this here to suppress warning in console
+            return;
+        }
+        for (int i = 0; i < hand.Count; i++)
+        {
+            Transform card = fiveCardLayout.transform.GetChild(i);
+            hand[i].transform.localPosition = card.localPosition;
+            hand[i].transform.localRotation = card.localRotation;
+            hand[i].transform.localScale = card.localScale;
+        }
+
+        Transform iconTrans = fiveCardLayout.transform.Find("Icon");
+        icon.transform.localPosition = iconTrans.localPosition;
+        icon.transform.localScale = iconTrans.localScale;
+    }
 }
